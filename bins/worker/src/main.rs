1c0f2e85ea41242a6f1fc4412f22450949e2e12f
Albin Coquereau <pro.acoquer@protonmail.com>
2023-05-11 15:37:23 +0200

    add dummy worker-bin

--- bins/worker/src/main.rs
+++ bins/worker/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use messages::Answer;
+use messages::Task;
+use serde::{Deserialize, Serialize};
+
+use clap::Parser;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long, default_value_t = 4242)]
+    port: u16,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value = "localhost")]
+    addr: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let port = args.port.to_string();
+
+    let addr = format!("{}:{}", args.addr, port);
+    println!("try to connect to {}", addr);
+
+    let mut stream = TcpStream::connect(addr)?;
+
+    let mut binary_size = [0_u8; 8];
+    stream.read_exact(&mut binary_size)?;
+    let size = usize::from_be_bytes(binary_size);
+
+    let mut msg = vec![0_u8; size];
+    stream.read_exact(&mut msg)?;
+    let task: messages::Task = serde_json::from_slice(&msg).unwrap();
+    use worker::handle_task;
+
+    let answer = handle_task(task);
+    let _serialized_answer = serde_json::to_vec(&answer).unwrap();
+
+    Ok(())
 }
