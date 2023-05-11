7637720c5083ebeccb0b6098f1a3b9dd5417765b
Raphaël Proust <code@bnwr.net>
2023-05-11 11:05:50 +0200

    types

--- libs/fractal/src/lib.rs
+++ libs/fractal/src/lib.rs
@@ -1,3 +1,16 @@
+pub struct Point { }
+pub struct Intensity { module: f64, divergence: u16 }
+
+pub trait Fractal {
+    fn eval(&self, _: u16, _: Point) -> Intensity {
+        return Intensity {module:0., divergence:0}
+    }
+}
+pub struct Julia { c: Point }
+
+impl Fractal for Julia {
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
