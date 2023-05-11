a61482417cc1043e3476382879c134f230fce676
Raphaël Proust <code@bnwr.net>
2023-05-11 11:42:50 +0200

    Julia implementation

--- libs/complex/src/lib.rs
+++ libs/complex/src/lib.rs
@@ -1,6 +1,24 @@
+#[derive(Clone)]
 pub struct Complex {
     pub re: f64,
     pub im: f64,
+}
+
+pub fn sqmodule(x: &Complex) -> f64 {
+    (x.re * x.re) + (x.im * x.im)
+}
+
+pub fn sq(x: Complex) -> Complex {
+    Complex {
+        re: (x.re * x.re) - (x.im * x.im),
+        im: 2. * (x.re * x.im),
+    }
+}
+pub fn add(x: Complex, c: &Complex) -> Complex {
+    Complex {
+        re: x.re + c.re,
+        im: x.im + x.im,
+    }
 }
 
 #[cfg(test)]
--- libs/fractal/src/lib.rs
+++ libs/fractal/src/lib.rs
@@ -7,9 +7,25 @@ pub trait Fractal {
         return Intensity {module:0., divergence:0}
     }
 }
-pub struct Julia { c: Complex }
+pub struct Julia { c: Complex, divergence_threshold_square: f64 }
+
+impl Julia {
+    fn next(&self, x:Complex) -> Complex {
+        complex::add(complex::sq(x), &self.c)
+    }
+}
 
 impl Fractal for Julia {
+    fn eval(self:&Julia, maxiter:u16, x:Complex) -> Intensity {
+        let mut divergence : u16 = 0;
+        let mut acc = x.clone();
+        while divergence < maxiter && complex::sqmodule(&acc) < self.divergence_threshold_square {
+            divergence = divergence + 1;
+            acc = self.next(acc)
+        };
+        let module = complex::sqmodule(&acc);
+        Intensity { module, divergence }
+    }
 }
 
 #[cfg(test)]
